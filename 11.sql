949d0f5afb0402d550a41201e902bd69fbc5d0aa
YoannLucido <57752806+YoannLucido@users.noreply.github.com>
2020-02-18 17:06:19 +0100

    Delete 11.sql

--- 11.sql
+++
@@ -1,7 +0,0 @@
-select departements.name, population2017.Population_légale_2017
-from departements join cities 
-on departements.region_code = cities.department_code
-join population2017
-on cities.insee_code = population2017.Code
-
-where departements.name like '%Bouches%'
